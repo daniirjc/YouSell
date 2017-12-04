a7c144b20a98d66685e7bd203dca821570692e7f
Daniel Rajic <danielrajicc@gmail.com>
2017-12-04 12:54:31 +0100

    csrf token added

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { postLoginDetails } from '../api/login';
+import { postLoginDetails, fetchToken } from '../api/login';
 
 const bg = {
     backgroundColor: "rgba(0,0,0, 0.5)",
@@ -26,7 +26,10 @@ class LoginForm extends React.Component {
         super();
         this.state = {
             username: '',
-            password: ''
+            password: '',
+            loading: false,
+            token: '',
+            error: false,
         };
         this.handleUserInput = this.handleUserInput.bind(this);
         this.handlePasswordInput = this.handlePasswordInput.bind(this);
@@ -37,37 +40,64 @@ class LoginForm extends React.Component {
         this.setState({username: event.target.value});
     }
 
+
     handlePasswordInput(event) {
         this.setState({password: event.target.value});
 
     }
 
+    componentWillMount() {
+        this.fetchTokenOnStart();
+    }
+
     onLogin(e) {
         e.preventDefault();
-        postLoginDetails(this.state.username, this.state.password).then(res => console.log(res));
+        this.setState({loading: true})
+        postLoginDetails(this.state.username, this.state.password, () => { this.setState({ loading: false }) })
     }
+
+    fetchTokenOnStart = async  () => {
+        this.setState({ error: false })
+        try {
+            const res = await fetchToken();
+            const data = await res.json();
+            if (data) {
+                console.log("token", data);
+                this.setState({ token: data._csrf })
+            }
+        } catch(e) {
+            console.log(e)
+            this.setState({ error: true })
+        }
+
+    };
 
 
     render () {
         return (
-                <form id="loginform" className="form-horizontal" method="post" onSubmit={this.onLogin}>
-                    <label style={head}>
-                        LOGIN
-                    </label>
-                    <div style={{marginBottom: 25}} className="input-group">
-                        <span  style={glybg} className="input-group-addon"><i className="glyphicon glyphicon-user white"/></span>
-                        <input id="login-username" type="text" style={bg} className="form-control" name="username"  value={this.state.username} onChange={this.handleUserInput} placeholder="username" required/>
-                    </div>
+            this.state.loading ? <h1>LOADING</h1> :
+                (
+                    <form id="loginform" className="form-horizontal" method="post" onSubmit={this.onLogin}>
+                        <input name="_csrf" value={this.state.token} type="hidden" readOnly={true} />
+                        <label style={head}>
+                            LOGIN
+                        </label>
+                        <div style={{marginBottom: 25}} className="input-group">
+                            <span  style={glybg} className="input-group-addon"><i className="glyphicon glyphicon-user white"/></span>
+                            <input id="login-username" type="text" style={bg} className="form-control" name="username"  value={this.state.username} onChange={this.handleUserInput} placeholder="username" required/>
+                        </div>
 
-                    <div style={{marginBottom: 25}} className="input-group">
-                        <span  style={glybg} className="input-group-addon"><i className="glyphicon glyphicon-lock white"/></span>
-                        <input id="login-password" type="password"  style={bg} className="form-control" value={this.state.password} onChange={this.handlePasswordInput} name="password" placeholder="password" required/>
-                    </div>
+                        <div style={{marginBottom: 25}} className="input-group">
+                            <span  style={glybg} className="input-group-addon"><i className="glyphicon glyphicon-lock white"/></span>
+                            <input id="login-password" type="password"  style={bg} className="form-control" value={this.state.password} onChange={this.handlePasswordInput} name="password" placeholder="password" required/>
+                        </div>
 
-                    <div style={{marginTop:10, width: "100%"}} className="input-group">
-                        <button type="submit" className="btn btn-success btn-block">LOGIN</button>
-                    </div>
-                </form>
+                        <div style={{marginTop:10, width: "100%"}} className="input-group">
+                            <button type="submit" className="btn btn-success btn-block">LOGIN</button>
+                        </div>
+                    </form>
+                )
+
 
 
         );
--- src/api/login.js
+++ src/api/login.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ENV from './env';
 
 
-export function postLoginDetails(username, password) {
+export function postLoginDetails(username, password, cb) {
     const url = 'http://' + ENV.host + ':' + ENV.port + '/login';
     const formData = new FormData();
     formData.append('username', username);
@@ -10,6 +10,11 @@ export function postLoginDetails(username, password) {
     return fetch(url, {
         method: 'POST',
         body: formData
-    }).then(response => response.json())
+    }).then(res => res.json()).then((data) => { cb(); console.log(data); return data; })
         .catch(e => console.log(e))
+}
+
+export function fetchToken() {
+    const url = 'http://' + ENV.host + ':' + ENV.port;
+    return fetch(url)
 }
